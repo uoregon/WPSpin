9b5033f8bed971d3dbd903d8eb4a17ca200b850c
Gaston Figueroa <gastonfig@gmail.com>
2012-11-26 10:37:53 -0800

    Fixing removing '.'from the track class.

--- assets/playlist_item.js
+++ assets/playlist_item.js
@@ -1,3 +1,3 @@
 var Playlist = Playlist || {};
 
-Playlist.PlaylistItem = '<p><p class=".track"><%= track %></p><span class=".artist"><%= artist %></span</p>';
+Playlist.PlaylistItem = '<p><p class="track"><%= track %></p><span class=".artist"><%= artist %></span</p>';
