4fd346faf0c9b5803bd422f9670ba122afe8303d
Joshua Rose <joshrose@uoregon.edu>
2012-12-05 13:33:14 -0800

    Initial migration from old playlist app.

--- functions.php
+++ functions.php
@@ -1,35 +1,39 @@
-<?php namespace KWVAPlaylist;
+<?php namespace WPSpin;
    /*
-   Plugin Name: KWVA Playlist
-   Plugin URI: http://github.com/emumark/kwva-playlist
-   Description: Provides the playlist for KWVA as fed from Spinitron
+   Plugin Name: WPSpin
+   Plugin URI: http://github.com/emumark/WPSpin
+   Description: Provides Wordpress Spinitron integraton through to SPINPAPI API
    Version: 0.5
    Author: Michael A Tomcal - University of Oregon - EMU Marketing
    Author URI: http://marketing.uoregon.edu
-   License: GPL2
+   License: MIT
    Copyright 2012  University of Oregon (email : emumark@uoregon.edu)
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License, version 2, as 
-   published by the Free Software Foundation.
+   Permission is hereby granted, free of charge, to any person obtaining a copy 
+   of this software and associated documentation files (the "Software"), to deal 
+   in the Software without restriction, including without limitation the rights 
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+   of the Software, and to permit persons to whom the Software is furnished to do so, 
+   subject to the following conditions:
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+   The above copyright notice and this permission notice shall be included in 
+   all copies or substantial portions of the Software.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-require_once 'kwva-playlist.php';
+require_once 'playlist.php';
 
-function init_kwva_widget() {
-	register_widget("KWVAPlaylist\KWVAPlaylist");
+function init_playlist_widget() {
+	register_widget("WPSpin\WPSPlaylist");
 }
 
-add_action( 'widgets_init', 'KWVAPlaylist\init_kwva_widget');
+add_action( 'widgets_init', 'WPSpin\init_playlist_widget');
 
 function script_registry($name_location_array) {
 	foreach ($name_location_array as $name => $location) {
@@ -41,7 +45,7 @@ function script_registry($name_location_array) {
 
 
 function playlist_scripts() {
-	$url = plugins_url('kwva-playlist');
+	$url = plugins_url('WPSpin');
 	$scripts_array = array(
 	    "underscore" => 'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.2/underscore-min.js',
 	    "backbone" => 'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.2/backbone-min.js',
@@ -58,7 +62,7 @@ function playlist_scripts() {
 
 }    
  
-add_action('wp_enqueue_scripts', 'KWVAPlaylist\playlist_scripts');
+add_action('wp_enqueue_scripts', 'WPSpin\playlist_scripts');
 
 
 ?>
--- playlist.php
+++ playlist.php
@@ -1,21 +1,21 @@
-<?php namespace KWVAPlaylist;
+<?php namespace WPSpin;
 
-class KWVAPlaylist extends \WP_Widget { 
+class WPSPlaylist extends \WP_Widget { 
 
 	public function __construct() {
 		parent::__construct( 
-		'kwva_playlist',
-	        'KWVA Playlist',
+		'wps_playlist',
+	        'WPSpin Playlist',
 		array(
-		    'classname' => 'kwva_playlist',
-		    'description' => __('Use this widget For KWVA Playlist Updates From The Spinitron RSS Feed'),
+		    'classname' => 'wps_playlist',
+		    'description' => __('Use this widget For Playlist Updates From Spinitron'),
 		    )
 		);
 	}
 
 	public function widget($args, $instance) {
 		extract($args);
-		$title = apply_filters( 'widget_title', 'KWVA Playlist' );
+		$title = apply_filters( 'widget_title', 'Now Playing' );
 		echo $before_widget;
 		if ( ! empty( $title ) )
 			echo $before_title . $title . $after_title;
