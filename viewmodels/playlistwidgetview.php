7e6f58e3b0480d2bb6f591445a80dd9b138d5bf4
Michael A Tomcal <michael@twoeyetech.com>
2013-01-29 15:29:44 -0800

    Changed playlist name to 'playlist'

--- viewmodels/playlistwidgetview.php
+++ viewmodels/playlistwidgetview.php
@@ -15,7 +15,7 @@ class PlaylistWidgetView extends \WP_Widget {
 
   public function widget($args, $instance) {
     extract($args);
-    $title = apply_filters( 'widget_title', 'Now Playing' );
+    $title = apply_filters( 'widget_title', 'Playlist' );
     echo $before_widget;
     if ( ! empty( $title ) ) {
       echo $before_title . $title . $after_title;
