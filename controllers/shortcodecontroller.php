8049e3480ff0742f1fe6a93b91306183b08b94c1
Joshua Rose <joshrose@uoregon.edu>
2013-03-22 15:41:52 -0700

    Simple implementation of weekly schedule shortcode

--- controllers/shortcodecontroller.php
+++ controllers/shortcodecontroller.php
@@ -6,11 +6,22 @@ class ShortcodeController extends ControllerAbstract
   public static function initActions()
   {
     add_shortcode('wpspin-now-playing', 'WPSpin\ShortcodeController::nowPlaying');
+    add_shortcode('wpspin-show-schedule', 'WPSpin\ShortcodeController::showSchedule');
   }
 
   public static function nowPlaying()
   {
     include __DIR__ . '/../views/nowplaying.php';
+  }
+
+  public static function showSchedule()
+  {
+  	$data = file_get_contents('http://spinitron.com/radio/playlist.php?station=kwva&show=schedule&ptype=s');
+	$stylesheet = plugin_dir_url( __FILE__ ) . '../views/showschedule.css';
+
+	// link stylesheet from view and render processed data
+	print('<link rel="stylesheet" href="'. $stylesheet .'" />');
+	print($data);
   }
 
 }
