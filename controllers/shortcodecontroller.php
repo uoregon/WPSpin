e3765ac9e4de3b3268d951ef6fef0480fbb39781
Joshua Rose <joshrose@uoregon.edu>
2013-03-25 13:09:52 -0700

    Schedule shortcode controller removes links.

--- controllers/shortcodecontroller.php
+++ controllers/shortcodecontroller.php
@@ -20,6 +20,13 @@ class ShortcodeController extends ControllerAbstract
   	$data = file_get_contents('http://spinitron.com/radio/playlist.php?station=' . $station . '&show=schedule&ptype=s');
 	$stylesheet = plugin_dir_url( __FILE__ ) . '../views/showschedule.css';
 
+	// Remove links
+	// Links look like: <a href="playlist.php?station=kwva&amp;ptype=s&amp;showid=380" title="Democracy Now! playlists">Democracy Now!</a>
+	// TODO: Figure out how to make these links go to our custom posts for show/dj. Something like:
+	// $data = preg_replace('/playlist.php?.*showid=/', '?page_id=', $data);
+	$data = preg_replace('/<a\b[^>]*>/', '', $data);
+	$data = preg_replace('/<\/a>/', '', $data);
+
 	// link stylesheet from view and render processed data
 	print('<link rel="stylesheet" href="'. $stylesheet .'" />');
 	print($data);
