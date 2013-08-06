e6afc5c7d5a899d874534f4649f57bb697ec619b
Joshua Rose <joshrose@uoregon.edu>
2013-08-06 13:36:41 -0700

    Added shortcode for playlist listings.

--- controllers/shortcodecontroller.php
+++ controllers/shortcodecontroller.php
@@ -7,6 +7,7 @@ class ShortcodeController extends ControllerAbstract
   {
     add_shortcode('wpspin-now-playing', 'WPSpin\ShortcodeController::nowPlaying');
     add_shortcode('wpspin-show-schedule', 'WPSpin\ShortcodeController::showSchedule');
+    add_shortcode('wpspin-playlist-list', 'WPSpin\ShortcodeController::playlistList');
   }
 
   public static function nowPlaying()
@@ -17,6 +18,12 @@ class ShortcodeController extends ControllerAbstract
   public static function showSchedule()
   {
     include __DIR__ . '/../views/showschedule.php';
+  }
+
+  public static function playlistList($atts)
+  {
+    $output = include __DIR__ . '/../views/playlistList.php';
+    return $output;
   }
 
 }
---
+++ views/playlistList.php
@@ -0,0 +1,31 @@
+<?php namespace WPSpin;
+/**
+ * Lists previous playlists given a show ID
+ */
+
+$args = shortcode_atts(array( 	
+								'show_id' => false,
+								'number' => 10
+							), $atts);
+if(!$args['show_id']) {
+	return "Error: Show ID not specified in shortcode.";
+} else {
+	$output = '';
+	// Get list from Spinitron
+	$spinpapi = SpinConnectSingleton::getInstance();
+	$query['method'] = "getPlaylistsInfo";
+	$query['ShowID'] = $args['show_id'];
+	$query['Num'] = $args['number'];	
+	
+	$results = $spinpapi->query($query);
+
+	foreach($results['results'] as $playlist) {
+		$startTime = strtotime($playlist['PlaylistDate'] . ' ' . $playlist['OnairTime']);
+		$endTime = strtotime($playlist['PlaylistDate'] . ' ' . $playlist['OffairTime']);
+		$output .= date('M jS g:i a', $startTime) . ' to ' . date('g:i a', $endTime) . '<br />';
+	}
+
+	return $output;
+}
+
+?>
