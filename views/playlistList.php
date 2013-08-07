59e3b167c451cf91197c3fe6e84c19bacf1c3eaa
Joshua Rose <joshrose@uoregon.edu>
2013-08-07 16:13:13 -0700

    Link playlist items through to Spinitron.

--- views/playlistList.php
+++ views/playlistList.php
@@ -15,14 +15,16 @@ if(!$args['show_id']) {
 	$spinpapi = SpinConnectSingleton::getInstance();
 	$query['method'] = "getPlaylistsInfo";
 	$query['ShowID'] = $args['show_id'];
-	$query['Num'] = $args['number'];	
+	$query['Num'] = $args['number'];
 	
 	$results = $spinpapi->query($query);
 
 	foreach($results['results'] as $playlist) {
 		$startTime = strtotime($playlist['PlaylistDate'] . ' ' . $playlist['OnairTime']);
 		$endTime = strtotime($playlist['PlaylistDate'] . ' ' . $playlist['OffairTime']);
+		$output .= '<a href="http://spinitron.com/radio/playlist.php?station=kwva&playlist=' . $playlist['PlaylistID'] . '">';
 		$output .= date('M jS g:i a', $startTime) . ' to ' . date('g:i a', $endTime) . '<br />';
+		$output .= '</a>';
 	}
 
 	return $output;
